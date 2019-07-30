e274330e1ea37138329cb3394f9336492fe91ed2
Matin M <matin@massoudi.net>
2019-07-30 13:12:05 -0700

    Added contour identification

--- OpenCVStanford.py
+++ OpenCVStanford.py
@@ -1,11 +1,17 @@
 import cv2
 import sys
 from matplotlib import pyplot as plt
+import numpy as np
+import ctypes
+
+def Mbox(title, text, style):
+  return cytypes.windll.user32.MessageBox32.MessageBoxW(0, text, title, style)
+
+Mbox("vars","input",1)
 
 # read original image
 filename = sys.argv[1]
-#The pixels of value t will be turned 'off'
-t = int(sys.argv[2])
+
 print("Filename and threshold value noted.  ")
 #image input. 
 image = cv2.imread(filename)
@@ -30,11 +36,11 @@ def histogram(filename):
         histSize = [256],
         ranges = [0,256])
     
-    print(histogram)
+    #print(histogram)
 
     # configure and draw the histogram figure
     plt.figure()
-    plt.title("Matin's Greyscale Histogram")
+    plt.title("Greyscale Histogram Analysis")
     plt.xlabel("Pixel Value")
     plt.ylabel("Pixels")
     plt.xlim([0, 256]) # <- named arguments do not work here
@@ -43,13 +49,30 @@ def histogram(filename):
     plt.show()
 
     #return histogram
+    return histogram
+
+def histogramAnalysis(histogram):
+    maxVal = np.amax(histogram)
+    return type(maxVal)
+
 
     
 #calling the histogram function
 histogram(filename)
+#print(histogramAnalysis(histogram(filename)))
+# create binary image
+# create binary image
 
-# create binary image
-# create binary image
+#The pixels of value t will be turned 'off'
+#Fixed level thresholding
+t = int(input("Enter threshold value"))
+
+def find_contours(binary):
+    #return second parameter in tuple, ignore rest of returnVals.
+    intermediary ,contours = cv2.findContours(image = binary, mode = cv2.RETR_EXTERNAL, method = cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+
+#Thresholding and image processing. 
 gray = cv2.cvtColor(
     src = image, 
     code = cv2.COLOR_BGR2GRAY)
@@ -61,9 +84,21 @@ t, binary = cv2.threshold(
     src = blur,
     thresh = t, 
     maxval = 255, 
-    type = cv2.THRESH_BINARY)
+    type = cv2.THRESH_BINARY_INV)
 
+contour_array = find_contours(binary)
 
+print("contours found")
+print(len(contour_array))
 
-# main function. 
+cv2.drawContours(image = binary, 
+    contours = contour_array, 
+    contourIdx = -1, 
+    color = (0, 0, 255), 
+    thickness = 5)
+
+cv2.imshow("processed image",binary)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 
