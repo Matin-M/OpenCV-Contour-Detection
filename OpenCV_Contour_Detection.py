50b137cd86a54d020828404f48f2cf5c0f75ef8b
Matin Massoudi <matinmassoudi@Matins-MacBook-Pro.local>
2019-08-01 11:27:04 -0700

    Cleaned up code

--- OpenCV_Contour_Detection.py
+++ OpenCV_Contour_Detection.py
@@ -10,7 +10,7 @@ import numpy as np
 #Mbox("vars","input",1)
 
 # read original image
-filename = sys.argv[1]
+filename = "01039201201150505544528.jpg"
 
 print("Filename and threshold value noted.  ")
 #image input. 
@@ -18,26 +18,11 @@ image = cv2.imread(filename)
 
 
 def histogram(filename):
-    processImage = cv2.imread(
-        #filename declaration
-        filename = filename, 
-        #greyscale preprocessing
-        flags = cv2.IMREAD_GRAYSCALE)
+    processImage = cv2.imread(filename = filename, flags = cv2.IMREAD_GRAYSCALE)
 
     #histogram is a one dimensional numpy array. 
-    histogram = cv2.calcHist(
-        #list of images.
-        images = [processImage],
-        #list of channels for image examination. 
-        channels = [0],
-        #no mask.
-        mask = None,
-        #Number of bins for the histogram. 
-        histSize = [256],
-        ranges = [0,256])
+    histogram = cv2.calcHist(images = [processImage], channels = [0], mask = None, histSize = [256], ranges = [0,256])
     
-    #print(histogram)
-
     # configure and draw the histogram figure
     plt.figure()
     plt.title("Greyscale Histogram Analysis")
@@ -75,18 +60,9 @@ def find_contours(binary):
     return contours
 
 #Thresholding and image processing. 
-gray = cv2.cvtColor(
-    src = image, 
-    code = cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(
-    src = gray, 
-    ksize = (5, 5), 
-    sigmaX = 0)
-(t, binary) = cv2.threshold(
-    src = blur,
-    thresh = t, 
-    maxval = 255, 
-    type = cv2.THRESH_BINARY_INV)
+gray = cv2.cvtColor(src = image, code = cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(src = gray, ksize = (5, 5), sigmaX = 0)
+(t, binary) = cv2.threshold(src = blur, thresh = t, maxval = 255, type = cv2.THRESH_BINARY_INV)
 
 contour_array = find_contours(binary)
 
